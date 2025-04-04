f2c45f590e9b399cf7c9fafd874e9cda8b2c2d22
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2025-04-04 21:20:42 +0330

    chore: rename project to ng-ui-lab

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello-angular-ui' title`, () => {
+  it(`should have the 'ng-ui-lab' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-angular-ui');
+    expect(app.title).toEqual('ng-ui-lab');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-angular-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-ui-lab');
   });
 });
