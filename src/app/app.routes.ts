769c86014ea78ddd40eea7ed3cfdf7e18b6730a8
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2025-04-05 16:09:34 +0330

    feat(layout): add main layout with header and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,25 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'welcome',
-        pathMatch: 'full'
-    },
-    {
-        path: 'welcome',
-        loadComponent: () => import('./features/welcome/welcome.component').then(m => m.WelcomeComponent)
-    },
-    {
-        path: 'counter',
-        loadComponent: () => import('./features/ccounter/ccounter.component').then(m => m.CcounterComponent)
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'welcome',
+                pathMatch: 'full'
+            },
+            {
+                path: 'welcome',
+                loadComponent: () => import('./features/welcome/welcome.component').then(m => m.WelcomeComponent)
+            },
+            {
+                path: 'counter',
+                loadComponent: () => import('./features/ccounter/ccounter.component').then(m => m.CcounterComponent)
+            }
+        ]
     }
+
 ];
