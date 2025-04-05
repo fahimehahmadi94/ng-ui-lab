a21e5c4ea1698dba88529d9254683bbbe3239d8f
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2025-04-05 11:56:42 +0330

    test: add unit tests for welcome component

--- src/app/features/welcome/welcome.component.spec.ts
+++ src/app/features/welcome/welcome.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeComponent } from './welcome.component';
+import { By } from '@angular/platform-browser';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
@@ -9,15 +10,32 @@ describe('WelcomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [WelcomeComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('باید متن پیش‌فرض Welcome Page رو نمایش بده', () => {
+    const title = fixture.nativeElement.querySelector('h2');
+    expect(title?.textContent).toContain('Welcome Page');
   });
+  it('باید پیام Hello وقتی name تنظیم بشه نمایش داده شود', () => {
+    component.name.set('Fahimeh');
+    fixture.detectChanges();
+
+    const message = fixture.nativeElement.querySelector('p');
+    expect(message?.textContent).toContain('Hello, Fahimeh!');
+  });
+
+  it('باید مقدار input را تغییر دهد وقتی کاربر تایپ می‌کند', () => {
+    const input = fixture.debugElement.query(By.css('input')).nativeElement as HTMLInputElement;
+    input.value = 'Fahimeh';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(component.name()).toBe('Fahimeh')
+  });
+
 });
