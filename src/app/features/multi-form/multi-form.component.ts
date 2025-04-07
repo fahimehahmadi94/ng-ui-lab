72e2ae5df3a26a9423e9a5a6a5b19f087c9f32da
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2025-04-07 23:17:17 +0330

    feat(form): add MultiFormComponent with route

---
+++ src/app/features/multi-form/multi-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './multi-form.component.html',
+  styleUrl: './multi-form.component.scss'
+})
+export class MultiFormComponent {
+
+}
